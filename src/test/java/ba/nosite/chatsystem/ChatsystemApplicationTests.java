ce82d2cf9dad34c24bfac49a82cfadd31698f2c0
Ivan Mijic <ivan.mijic@stu.ibu.edu.ba>
2023-10-05 09:37:34 +0200

    Initial commit

---
+++ src/test/java/ba/nosite/chatsystem/ChatsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.nosite.chatsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
